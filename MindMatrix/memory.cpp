225c639739a3f09d4e10c368100ff37561524629
Ziglot <zigatase@gmail.com>
2024-03-01 14:53:00 +0300

    v0.0.3

--- MindMatrix/memory.cpp
+++ MindMatrix/memory.cpp
@@ -1,1 +1,24 @@
 #include "memory.h"
+
+
+PVOID GetSystemModuleBase(const char* module_name)
+{
+	ULONG bytes = 0;
+	NTSTATUS status = ZwQuerySystemInformation(SystemModuleInformation, NULL, bytes, &bytes);
+}
+
+PVOID GetSystemModuleExport(const char* module_name, LPCSTR routine_name)
+{
+
+}
+
+
+bool WriteMemory(void* address, void* buffer, size_t size)
+{
+
+}
+
+bool WriteToReadOnlyMemory(void* address, void* buffer, size_t size)
+{
+
+}
