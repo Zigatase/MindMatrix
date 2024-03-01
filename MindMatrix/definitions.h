954288ecc081b24d3149a6c80080f191f0b538bc
Ziglot <zigatase@gmail.com>
2024-03-01 14:51:04 +0300

    v0.0.3

---
+++ MindMatrix/definitions.h
@@ -0,0 +1,85 @@
+#ifndef DEFENITIONS_H_
+#define DEFENITIONS_H_
+
+#include <ntdef.h>
+#include <ntifs.h>
+#include <ntddk.h>
+#include <windef.h>
+#include <wdm.h>
+
+#pragma comment(lib, "ntoskrnl.lib")
+
+typedef enum _SYSTEM_INFORMATION_CLASS
+{
+	SystemBasicInformation,
+	SystemProcessorInformation,
+	SystemPerformanceInformation,
+	SystemTimeOfDayInformation,
+	SystemPathInformation,
+	SystemProcessInformation,
+	SystemCallCountInformation,
+	SystemDeviceInformation,
+	SystemProcessorPerformanceInformation,
+	SystemFlagsInformation,
+	SystemCallTimeInformation,
+	SystemModuleInformation = 0x0B
+} SYSTEM_INFORMATION_CLASS, * PSYSTEM_INFORMATION_CLASS;
+
+typedef struct _RTL_PROCESS_MODULE_INFORMATION
+{
+	HANDLE Section;
+
+	PVOID MappedBase;
+	PVOID ImageBase;
+
+	ULONG ImageSize;
+	ULONG Flags;
+
+	USHORT LoadOrderIndex;
+	USHORT InitOrderIndex;
+	USHORT LoadCount;
+	USHORT OffsetToFileName;
+
+	UCHAR FullPathName[256];
+} RTL_PROCESS_MODULE_INFORMATION, * PRTL_PROCESS_MODULE_INFORMATION;
+
+typedef struct _RTL_PROCESS_MODULES
+{
+	ULONG NumberOfModules;
+
+	RTL_PROCESS_MODULE_INFORMATION Modules[1];
+} RTL_PROCESS_MODULE, * PRTL_PROCESS_MODULES;
+
+extern "C" _declspec(dllimport)
+NTSTATUS NTAPI ZwProtectVirtualMemory(
+	HANDLE ProcessHandle,
+	PVOID * BaseAddress,
+	PULONG ProtectSize,
+	ULONG NewProtect,
+	PULONG OldProtect
+);
+
+extern "C" NTKERNELAPI
+PVOID NTAPI RtlFindRxporedRoutineByName(
+	_In_ PVOID ImageBase,
+	_In_ PCCH RoutineName
+);
+
+extern "C" NTSTATUS ZwQuerySystemInformation(ULONG InfoClass, PVOID Buffer, ULONG Length, PULONG ReturnLength);
+
+extern "C" NTKERNELAPI
+PPEB PsGetProcessPeb(
+	IN PEPROCESS Process
+);
+
+extern "C" NTSTATUS NTAPI MmCopyVirtualMemory(
+	PEPROCESS SourceProcess,
+	PVOID SourceAddress,
+	PEPROCESS TargetProcess,
+	PVOID TargetAddress,
+	SIZE_T BufferSize,
+	KPROCESSOR_MODE PreviousMode,
+	PSIZE_T ReturnSize
+);
+
+#endif // !DEFENITIONS_H_
